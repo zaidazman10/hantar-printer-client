eb0279ff3a6a3ad01b30db3b18e83badc7143562
zaidazman10 <zaidazman10@gmail.com>
2026-04-19 20:39:30 +0800

    fix: fallback to msedge for silent pdf printing

--- index.js
+++ index.js
@@ -710,7 +710,9 @@ async function printLabel(order) {
         const chromePaths = [
             'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
             'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
-            `${process.env.LOCALAPPDATA}\\Google\\Chrome\\Application\\chrome.exe`
+            `${process.env.LOCALAPPDATA}\\Google\\Chrome\\Application\\chrome.exe`,
+            'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
+            'C:\\Program Files\\Microsoft\\Edge\\Application\\msedge.exe'
         ];
 
         const sumatraPaths = [
